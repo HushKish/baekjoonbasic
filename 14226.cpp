625a24817504254103d79980c58c34f1ba731a1b
HushKish <eliace3749@gmail.com>
2022-02-24 13:22:56 +0900

    fc

--- 14226.cpp
+++ 14226.cpp
@@ -1,106 +1,106 @@
-#include<iostream>
-#include<queue>
-using namespace std;
-int scr[1000000] = {1};
-int cli[1000000];
-int cnt[1000000] = {1}; //초기값만 0으로 초기화되는 듯
-int idx = 1;
-int bfs(int n){
-    queue<pair<char,int> > q; //두번째인자는 인댁스
-    q.push(make_pair('c',1)); 
-    cli[idx] = 1;
-    scr[idx] = 1;
-    cnt[idx] = 1;
-    while(!q.empty()){
-        char wh = q.front().first; int pi = q.front().second; // past index
-        q.pop();
-        if(wh == 'c'){
-            //c추가할때
-            cnt[++idx] = cnt[pi] + 1;
-            cli[idx] =  scr[pi];
-            scr[idx] = scr[pi]; // 이렇게 옮겨주지 않으면 0인 값이 유지됨
-            q.push(make_pair('c',idx));
-            //p추가
-            cnt[++idx] = cnt[pi] + 1;
-            scr[idx] = cli[pi] + scr[pi];
-            cli[idx] = cli[pi];
-            // cout << scr[idx] << endl;
-            if(scr[idx] == n) return cnt[idx];
-            q.push(make_pair('p',idx));
+// #include<iostream>
+// #include<queue>
+// using namespace std;
+// int scr[1000000] = {1};
+// int cli[1000000];
+// int cnt[1000000] = {1}; //초기값만 0으로 초기화되는 듯
+// int idx = 1;
+// int bfs(int n){
+//     queue<pair<char,int> > q; //두번째인자는 인댁스
+//     q.push(make_pair('c',1)); 
+//     cli[idx] = 1;
+//     scr[idx] = 1;
+//     cnt[idx] = 1;
+//     while(!q.empty()){
+//         char wh = q.front().first; int pi = q.front().second; // past index
+//         q.pop();
+//         if(wh == 'c'){
+//             //c추가할때
+//             cnt[++idx] = cnt[pi] + 1;
+//             cli[idx] =  scr[pi];
+//             scr[idx] = scr[pi]; // 이렇게 옮겨주지 않으면 0인 값이 유지됨
+//             q.push(make_pair('c',idx));
+//             //p추가
+//             cnt[++idx] = cnt[pi] + 1;
+//             scr[idx] = cli[pi] + scr[pi];
+//             cli[idx] = cli[pi];
+//             // cout << scr[idx] << endl;
+//             if(scr[idx] == n) return cnt[idx];
+//             q.push(make_pair('p',idx));
             
-            if(scr[pi] != 0){
-                cnt[++idx] = cnt[pi] + 1;
-                scr[idx] = --scr[pi];
-                cli[idx] = cli[pi];
-            if(scr[idx] == n) return cnt[idx];
-                q.push(make_pair('d',idx));
-            //d추가
-            }
-        }
-        else if(wh == 'p'){
-            //c추가할때
-            cnt[++idx] = cnt[pi] + 1;
-            cli[idx] =  scr[pi];
-            scr[idx] = scr[pi];
-            q.push(make_pair('c',idx));
+//             if(scr[pi] != 0){
+//                 cnt[++idx] = cnt[pi] + 1;
+//                 scr[idx] = --scr[pi];
+//                 cli[idx] = cli[pi];
+//             if(scr[idx] == n) return cnt[idx];
+//                 q.push(make_pair('d',idx));
+//             //d추가
+//             }
+//         }
+//         else if(wh == 'p'){
+//             //c추가할때
+//             cnt[++idx] = cnt[pi] + 1;
+//             cli[idx] =  scr[pi];
+//             scr[idx] = scr[pi];
+//             q.push(make_pair('c',idx));
             
-            if(scr[pi] != 0){
-                cnt[++idx] = cnt[pi] + 1;
-                scr[idx] = --scr[pi];
-                cli[idx] = cli[pi];
-            if(scr[idx] == n) return cnt[idx];
-                q.push(make_pair('d',idx));
-            //d추가
-            }
-        }
-        else if(wh == 'd'){
-            if(cli[pi] != 0){
-                cnt[++idx] = cnt[pi] + 1;
-                scr[idx] = cli[pi] + scr[pi];
-                cli[idx] = cli[pi];
-            if(scr[idx] == n) return cnt[idx];
-                q.push(make_pair('p',idx));
-            } //p추가
+//             if(scr[pi] != 0){
+//                 cnt[++idx] = cnt[pi] + 1;
+//                 scr[idx] = --scr[pi];
+//                 cli[idx] = cli[pi];
+//             if(scr[idx] == n) return cnt[idx];
+//                 q.push(make_pair('d',idx));
+//             //d추가
+//             }
+//         }
+//         else if(wh == 'd'){
+//             if(cli[pi] != 0){
+//                 cnt[++idx] = cnt[pi] + 1;
+//                 scr[idx] = cli[pi] + scr[pi];
+//                 cli[idx] = cli[pi];
+//             if(scr[idx] == n) return cnt[idx];
+//                 q.push(make_pair('p',idx));
+//             } //p추가
 
-            cnt[++idx] = cnt[pi] + 1;
-            cli[idx] = scr[pi];
-            scr[idx] = scr[pi]; // 이렇게 옮겨주지 않으면 0인 값이 유지됨
-            q.push(make_pair('c',idx));//c추가
+//             cnt[++idx] = cnt[pi] + 1;
+//             cli[idx] = scr[pi];
+//             scr[idx] = scr[pi]; // 이렇게 옮겨주지 않으면 0인 값이 유지됨
+//             q.push(make_pair('c',idx));//c추가
 
-            if(scr[pi] != 0){
-                cnt[++idx] = cnt[pi] + 1;
-                scr[idx] = --scr[pi];
-                cli[idx] = cli[pi];
-            if(scr[idx] == n) return cnt[idx];             
-                q.push(make_pair('d',idx));
-            //d추가
-            }
+//             if(scr[pi] != 0){
+//                 cnt[++idx] = cnt[pi] + 1;
+//                 scr[idx] = --scr[pi];
+//                 cli[idx] = cli[pi];
+//             if(scr[idx] == n) return cnt[idx];             
+//                 q.push(make_pair('d',idx));
+//             //d추가
+//             }
             
-        }
+//         }
 
         
 
-    }
-    return 0;
+//     }
+//     return 0;
 
-}
-int main(void){
-    int n;
-    cin >> n;
-    cout << bfs(n);
-}
+// }
+// int main(void){
+//     int n;
+//     cin >> n;
+//     cout << bfs(n);
+// }
 
 #include <iostream>
 #include <tuple>
 #include <queue>
-#include <cstring>
+#include <string>
 using namespace std;
 int d[1001][1001];
 int main() {
     int n;
     cin >> n;
-    memset(d,-1,sizeof(d));
-    queue<pair<int,int>> q;
+    memset(d,-1,sizeof(d)); //전역변수를 초기화할 때 이렇게 해야함
+    queue<pair<int,int> > q;
     q.push(make_pair(1,0));
     d[1][0] = 0;
     while (!q.empty()) {
